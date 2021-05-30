88611b6b46c6b8032a90c9648a40d15a17682e55
Emmanuel Benson <emmanuel.benson@konga.com>
2021-05-30 17:57:48 +0100

    Add endpoint

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MoviesController;
 
+Route::get('/', [MoviesController::class, 'index'])->name('movies.index');
 Route::get('/movies', [MoviesController::class, 'index'])->name('movies.index');
 Route::get('/movies/{movie}', [MoviesController::class, 'show'])->name('movies.show');
